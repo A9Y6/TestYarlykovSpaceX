2309c826d7d904a98b2b6c6fc45c1de573151e16
A9Y6 <58468713+A9Y6@users.noreply.github.com>
2019-12-17 14:55:12 +0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+	<link rel="stylesheet" type="text/css" href="/styles.css" >
+	<title>Тестовое задание.Стажер. Ярлыков</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF8"/>
+</head>
+<body>
+	<div class="bg-head">
+		<div class="container block-head">
+			<div class="spaceX">
+				<a href="">
+					<img class="spaceX-img" src="/img/spaceX.png">
+				</a>
+			</div>
+			<div class="block-link">
+				<a href="" class="link-head"> Главная </a>
+				<a href="" class="link-head"> Технология</a>
+				<a href="" class="link-head"> График полетов </a>
+				<a href="" class="link-head"> Гарантии</a>
+				<a href="" class="link-head"> О компании</a>
+				<a href="" class="link-head"> Контакты </a>
+			</div>
+			<div class="block-menu">
+				<input type="checkbox" id="hmt" class="hidden-menu-ticker">
+				<label class="btn-menu" for="hmt">
+				  <span class="first"></span>
+				  <span class="second"></span>
+				  <span class="third"></span>
+				</label>
+				<ul class="hidden-menu">
+				  <li><a href="" class="link-menu">Главная</a></li>  
+				  <li><a href="" class="link-menu">Технология</a></li>
+				  <li><a href="" class="link-menu">График полетов</a></li>  
+				  <li><a href="" class="link-menu">Гарантии</a></li> 
+				  <li><a href="" class="link-menu">О компании</a></li> 
+				  <li><a href="" class="link-menu">Контакты</a></li> 
+				</ul>
+			</div>
+		</div>
+	</div>
+		<div class="container block-body">
+				<div class="block-travel">
+					<button class="button-travel">Начать путешествие</button>
+				</div>
+				<div class="block-info">
+					<div class="info-text bg-top-left">
+						<span class="small-text">мы</span>
+						<h1 class="H-text">1</h1>
+						<span class="small-text">на рынке</span>
+					</div>
+					<div class="info-text bg-top-right">
+						<span class="small-text">гаранитруем</span>
+						<h1 class="H-text">50%</h1>
+						<span class="small-text">безопасность</span>
+					</div>
+					<div class="info-text bg-bottom-left">
+						<span class="small-text">календарик за</span>
+						<div class="div-year">
+							<h1 class="H-text">2001</h1>
+							<span>г.</span>
+						</div>
+						<span class="small-text">в подарок</span>
+					</div>
+					<div class="info-text bg-bottom-right">
+						<span class="small-text">путешествие</span>
+						<h1 class="H-text">597</h1>
+						<span class="small-text">дней</span>
+					</div>
+				</div>
+		</div>
+</body>
+</html>
